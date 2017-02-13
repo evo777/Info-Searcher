0cfada982e872ee7f06c5c0edfd6b0b8db867c1f
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-02-13 13:32:41 -0800

    Added SearchForm component and rendered test message

---
+++ src/js/components/SearchForm.js
@@ -0,0 +1,15 @@
+var React = require('react');
+var AppActions = require('../actions/AppActions');
+var AppStore = require('../stores/AppStore');
+
+var SearchForm = React.createClass({
+  render: function(){
+    return(
+      <div>
+        Form
+      </div>
+    );
+  }
+});
+
+module.exports = SearchForm;
